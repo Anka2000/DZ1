2f080920ceecba851ff0caa90225766ee447ff28
Анна <dethklok1996@mail.ru>
2021-11-02 15:43:11 +0300

    Добавьте файлы проекта.

---
+++ DZ1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+namespace Domashechka
+{
+    static class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Zadanie.Error();
+
+        }
+        public static class Zadanie
+
+        {
+
+            public static void Error()
+            {
+                const string word = "Error";
+
+                using (StreamReader reader = new StreamReader("C:\\Users\\Анна\\Desktop\\log.log", Encoding.UTF8))
+                {
+                    Regex regex = new Regex("\\b" + word + "\\b", RegexOptions.IgnoreCase);
+                    string str = null;
+                    while ((str = reader.ReadLine()) != null)
+                    {
+
+                    }
+                    Console.ReadKey(true);
+                }
+            }
+
+        }
+    }
+}
