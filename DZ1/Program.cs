6b34f8405044ce311239051d4807a0580f08edaa
Анна <dethklok1996@mail.ru>
2021-11-02 15:43:11 +0300

    Добавьте файлы проекта.

---
+++ DZ1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+namespace Domashechka
+{
+    static class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Zadanie.Error();
+
+        }
+        public static class Zadanie
+
+        {
+
+            public static void Error()
+            {
+
+                using (StreamReader reader = new StreamReader("C:\\Users\\Анна\\Desktop\\log.log", Encoding.UTF8))
+                {
+
+                }
+            }
+
+        }
+    }
+}
